f5a23445949e8e60407aa86e5c8378cbba1cc486
Bryn Bennett <bryn.t.bennett@gmail.com>
2016-09-16 11:31:48 +1200

    Added content from first lab
    This is the starting mateial for the project

---
+++ JavaSimulation/Drawing.java
@@ -0,0 +1,130 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package ToWebSite;
+
+import ToWebSite.PointXY;
+import ecs100.UI;
+import java.awt.Color;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+
+public class Drawing {
+    private ArrayList<PointXY> path = new ArrayList();
+
+    public Drawing() {
+    }
+
+    public void add_point_to_path(double x, double y, boolean pen) {
+        PointXY new_point = new PointXY(x, y, pen);
+        this.path.add(new_point);
+        UI.printf("Pioint added.x=%f y=%f pen=%b New path size - %d\n", new Object[]{Double.valueOf(x), Double.valueOf(y), Boolean.valueOf(pen), Integer.valueOf(this.path.size())});
+    }
+
+    public void print_path() {
+        UI.printf("*************************\n", new Object[0]);
+
+        for(int i = 0; i < this.path.size(); ++i) {
+            double x0 = ((PointXY)this.path.get(i)).get_x();
+            double y0 = ((PointXY)this.path.get(i)).get_y();
+            boolean p = ((PointXY)this.path.get(i)).get_pen();
+            UI.printf("i=%d x=%f y=%f pen=%b\n", new Object[]{Integer.valueOf(i), Double.valueOf(x0), Double.valueOf(y0), Boolean.valueOf(p)});
+        }
+
+        UI.printf("*************************\n", new Object[0]);
+    }
+
+    public void draw() {
+        for(int i = 1; i < this.path.size(); ++i) {
+            PointXY p0 = this.get_drawing_point(i - 1);
+            PointXY p1 = this.get_drawing_point(i);
+            if(((PointXY)this.path.get(i)).get_pen()) {
+                UI.setColor(Color.BLUE);
+            } else {
+                UI.setColor(Color.LIGHT_GRAY);
+            }
+
+            UI.drawLine(p0.get_x(), p0.get_y(), p1.get_x(), p1.get_y());
+        }
+
+    }
+
+    public int get_path_size() {
+        return this.path.size();
+    }
+
+    public void path_raise_pen() {
+        ((PointXY)this.path.get(this.path.size() - 1)).set_pen(false);
+    }
+
+    public PointXY get_path_last_point() {
+        PointXY lp = (PointXY)this.path.get(this.path.size() - 1);
+        return lp;
+    }
+
+    public void save_path(String fname) {
+        try {
+            File e = new File(fname);
+            FileOutputStream is = new FileOutputStream(e);
+            OutputStreamWriter osw = new OutputStreamWriter(is);
+            BufferedWriter w = new BufferedWriter(osw);
+
+            for(int i = 1; i < this.path.size(); ++i) {
+                String str_out;
+                if(((PointXY)this.path.get(i)).get_pen()) {
+                    str_out = ((PointXY)this.path.get(i)).get_x() + " " + ((PointXY)this.path.get(i)).get_y() + " 1\n";
+                } else {
+                    str_out = ((PointXY)this.path.get(i)).get_x() + " " + ((PointXY)this.path.get(i)).get_y() + " 0\n";
+                }
+
+                w.write(str_out);
+            }
+
+            w.close();
+        } catch (IOException var8) {
+            UI.println("Problem writing to the file statsTest.txt");
+        }
+
+    }
+
+    public void load_path(String fname) {
+        String in_line = null;
+
+        try {
+            BufferedReader e = new BufferedReader(new FileReader(new File(fname)));
+            this.path.clear();
+
+            while((in_line = e.readLine()) != null) {
+                UI.println(in_line);
+                String[] tokens = in_line.split(" ");
+                UI.println("Number of tokens in line " + in_line + ": " + tokens.length);
+                UI.println("The tokens are:");
+                UI.printf("%s %s %s\n", new Object[]{tokens[0], tokens[1], tokens[2]});
+                double x = Double.parseDouble(tokens[0]);
+                double y = Double.parseDouble(tokens[1]);
+                boolean pen = Integer.parseInt(tokens[2]) == 1;
+                this.add_point_to_path(x, y, pen);
+            }
+        } catch (Exception var10) {
+            var10.printStackTrace();
+        }
+
+    }
+
+    public int get_drawing_size() {
+        return this.path.size();
+    }
+
+    public PointXY get_drawing_point(int i) {
+        PointXY p = (PointXY)this.path.get(i);
+        return p;
+    }
+}
---
+++ JavaSimulation/Main.java
@@ -0,0 +1,132 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package ToWebSite;
+
+import ToWebSite.Arm;
+import ToWebSite.Drawing;
+import ToWebSite.PointXY;
+import ToWebSite.ToolPath;
+import ecs100.UI;
+import ecs100.UIButtonListener;
+import ecs100.UIFileChooser;
+import ecs100.UIKeyListener;
+import ecs100.UIMouseListener;
+import java.awt.Color;
+
+public class Main {
+    private Arm arm;
+    private Drawing drawing;
+    private ToolPath tool_path;
+    private int state;
+
+    public Main() {
+        UI.initialise();
+        UI.addButton("xy to angles", this::inverse);
+        UI.addButton("Enter path XY", this::enter_path_xy);
+        UI.addButton("Save path XY", this::save_xy);
+        UI.addButton("Load path XY", this::load_xy);
+        UI.addButton("Save path Ang", this::save_ang);
+        UI.addButton("Load path Ang:Play", this::load_ang);
+        UI.setMouseMotionListener(this::doMouse);
+        UI.setKeyListener(this::doKeys);
+        this.arm = new Arm();
+        this.drawing = new Drawing();
+        this.run();
+        this.arm.draw();
+    }
+
+    public void doKeys(String action) {
+        UI.printf("Key :%s \n", new Object[]{action});
+        if(action.equals("b")) {
+            this.state = 3;
+        }
+
+    }
+
+    public void doMouse(String action, double x, double y) {
+        UI.clearGraphics();
+        String out_str = String.format("%3.1f %3.1f", new Object[]{Double.valueOf(x), Double.valueOf(y)});
+        UI.drawString(out_str, x + 10.0D, y + 10.0D);
+        if(this.state == 1 && action.equals("clicked")) {
+            this.arm.inverseKinematic(x, y);
+            this.arm.draw();
+        } else {
+            if((this.state == 2 || this.state == 3) && action.equals("moved")) {
+                this.arm.inverseKinematic(x, y);
+                this.arm.draw();
+                if(this.state == 2 && this.drawing.get_path_size() > 0) {
+                    new PointXY();
+                    PointXY lp = this.drawing.get_path_last_point();
+                    UI.setColor(Color.GRAY);
+                    UI.drawLine(lp.get_x(), lp.get_y(), x, y);
+                }
+
+                this.drawing.draw();
+            }
+
+            if(this.state == 2 && action.equals("clicked")) {
+                UI.printf("Adding point x=%f y=%f\n", new Object[]{Double.valueOf(x), Double.valueOf(y)});
+                this.drawing.add_point_to_path(x, y, true);
+                this.arm.inverseKinematic(x, y);
+                this.arm.draw();
+                this.drawing.draw();
+                this.drawing.print_path();
+            }
+
+            if(this.state == 3 && action.equals("clicked")) {
+                this.drawing.add_point_to_path(x, y, false);
+                this.arm.inverseKinematic(x, y);
+                this.arm.draw();
+                this.drawing.draw();
+                this.drawing.print_path();
+                this.state = 2;
+            }
+
+        }
+    }
+
+    public void save_xy() {
+        this.state = 0;
+        String fname = UIFileChooser.save();
+        this.drawing.save_path(fname);
+    }
+
+    public void enter_path_xy() {
+        this.state = 2;
+    }
+
+    public void inverse() {
+        this.state = 1;
+        this.arm.draw();
+    }
+
+    public void load_xy() {
+        this.state = 0;
+        String fname = UIFileChooser.open();
+        this.drawing.load_path(fname);
+        this.drawing.draw();
+        this.arm.draw();
+    }
+
+    public void save_ang() {
+        String fname = UIFileChooser.open();
+        this.tool_path.convert_drawing_to_angles(this.drawing, this.arm, fname);
+    }
+
+    public void load_ang() {
+    }
+
+    public void run() {
+        while(true) {
+            this.arm.draw();
+            UI.sleep(20.0D);
+        }
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+}
